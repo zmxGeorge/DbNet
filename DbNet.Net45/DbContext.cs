0c654b5390b95c302027b76838b3eddcf5423339
周鸣轩 <657427356@qq.com>
2017-10-13 18:54:19 +0800

    创建DbNet

---
+++ DbNet.Net45/DbContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbNet.Net45
+{
+    /// <summary>
+    /// 所有注入类都继承该类
+    /// </summary>
+    public abstract class DbContext
+    {
+        /// <summary>
+        /// 数据接口执行异常处理事件
+        /// </summary>
+        public static event Action<Exception> SqlException;
+
+        /// <summary>
+        /// 数据接口初始化事件
+        /// </summary>
+        public static event Action<IDbFunction> FunctionInit;
+
+        /// <summary>
+        /// 开始执行接口事件
+        /// </summary>
+        public static event Action<IDbFunction, string> ExecuteStart;
+
+        /// <summary>
+        /// 接口执行结束事件
+        /// </summary>
+        public static event Action<IDbFunction, string> ExecuteEnd;
+    }
+}
